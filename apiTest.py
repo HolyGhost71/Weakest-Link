6bf00e45ef068f5f9dd175dfd47ae2f0021fde80
psyjm22 <psyjm22@nottingham.ac.uk>
2024-08-16 17:05:27 +0100

    Removed test file

--- apiTest.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get('https://the-trivia-api.com/v2/questions')
-question = response.json()[0]
-
-print(question['category'])
-print(question['question'])
-print(question['correctAnswer'])
